d069b4b487847ac999d99b7b20b95f4ea24e41c7
stnwtr <simon.steinkellner.ss@gmail.com>
2020-12-03 09:02:39 +0100

    day03 solution

--- src/main/kotlin/at/stnwtr/aoc2020/Launcher.kt
+++ src/main/kotlin/at/stnwtr/aoc2020/Launcher.kt
@@ -3,9 +3,10 @@ package at.stnwtr.aoc2020
 import at.stnwtr.aoc2020.day00.Day00
 import at.stnwtr.aoc2020.day01.Day01
 import at.stnwtr.aoc2020.day02.Day02
+import at.stnwtr.aoc2020.day03.Day03
 
 fun main() {
-    val days = listOf(Day00(), Day01(), Day02())
+    val days = listOf(Day00(), Day01(), Day02(), Day03())
 
     for (index in days.indices) {
         days[index].run()
---
+++ src/main/kotlin/at/stnwtr/aoc2020/day03/Day03.kt
@@ -0,0 +1,34 @@
+package at.stnwtr.aoc2020.day03
+
+import at.stnwtr.aoc2020.shared.Day
+import java.nio.file.Files
+
+const val TREE = '#'
+
+class Forest(private val map: List<String>) {
+    private val width = map[0].length
+
+    fun countTrees(speedX: Int, speedY: Int): Int {
+        return (map.indices step speedY)
+            .map { (it / speedY * speedX) % width to it}
+            .filter { map[it.second][it.first] == TREE }
+            .count()
+    }
+
+    fun countTrees(speed: Pair<Int, Int>) = countTrees(speed.first, speed.second)
+}
+
+class Day03 : Day("03") {
+    private val forest = Forest(Files.readAllLines(inputFilePath))
+
+    override fun part1() {
+        println("Trees: ${forest.countTrees(3, 1)}")
+    }
+
+    override fun part2() {
+        listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2)
+            .map { forest.countTrees(it) }
+            .foldRight(1L, { trees, value -> trees * value})
+            .also { println("Trees: $it") }
+    }
+}
