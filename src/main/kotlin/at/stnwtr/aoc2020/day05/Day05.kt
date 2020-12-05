e3a48868c8b538f6aa1aa9a8bf625064a091284c
stnwtr <simon.steinkellner.ss@gmail.com>
2020-12-05 10:23:20 +0100

    updated string replacement

--- src/main/kotlin/at/stnwtr/aoc2020/day05/Day05.kt
+++ src/main/kotlin/at/stnwtr/aoc2020/day05/Day05.kt
@@ -4,9 +4,14 @@ import at.stnwtr.aoc2020.shared.Day
 import java.nio.file.Files
 
 class Day05 : Day("05") {
+    private fun String.replace(replacements: Map<Char, Char>): String {
+        var new = this
+        replacements.forEach { (from, to) -> new = new.replace(from, to) }
+        return new
+    }
+
     private val boardingPassIds = Files.readString(inputFilePath)
-        .replace('F', '0').replace('B', '1')
-        .replace('L', '0').replace('R', '1')
+        .replace(mapOf('F' to '0', 'B' to '1', 'L' to '0', 'R' to '1'))
         .split(System.lineSeparator())
         .map { it.substring(0, 7).toInt(2) to it.substring(7).toInt(2) }
         .map { it.first * 8 + it.second }
